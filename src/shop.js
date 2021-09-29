4755ac433d806df27011ca97a0688d172615e9d8
pas8 <whyiampas@gmail.com>
2021-09-29 18:52:34 +0300

    added some placeholder skeletons and some styling and basic markup

--- src/shop.js
+++ src/shop.js
@@ -3,12 +3,5 @@ import '@styles/shop.scss';
 import '@styles/_breadcrumb.scss';
 import '@components/double-range.web.js';
 
-import { get_double_range_values } from '@utils/get_double_range_values.util.js';
-const arr = get_double_range_values()
-arr;
-
-// setInterval(() => {
-  
-//   console.log(arr)
-  
-// }, 1000);
+// import { get_double_range_values } from '@utils/get_double_range_values.util.js';
+// const p = get_double_range_values()
--- src/utils/get_double_range_values.util.js
+++ src/utils/get_double_range_values.util.js
@@ -1,16 +1,17 @@
-import { use_debounce } from '@utils/use_debounce.util';
+// import { use_debounce } from '@utils/use_debounce.util';
 
 export const get_double_range_values = () => {
-  const g = document.getElementById('double-range');
+  // const g = document.getElementById('double-range');
 
-  // let j = [];
+  // let j = false;
 
-  [...g.children].slice(1).map((el) => {
-    console.log(el);
-    el.addEventListener('input', function () {
-      use_debounce(function () {
-        console.olog(';u');
-      }, 250);
-    });
-  });
+  // return () =>
+  //   [...g.children].slice(1).map((el) => {
+  //     el.addEventListener(
+  //       'change',
+  //       use_debounce(function () {
+  //         console.log(true);
+  //       }, 250)
+  //     );
+  //   });
 };
