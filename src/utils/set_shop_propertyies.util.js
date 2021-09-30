3af738a090b011088f712f14681a04aa896cf546
pas8 <whyiampas@gmail.com>
2021-09-30 14:09:39 +0300

    finally added   pagination logic to perfect (probably)

--- src/utils/set_shop_propertyies.util.js
+++ src/utils/set_shop_propertyies.util.js
@@ -2,10 +2,10 @@ import { use_validation_of_products } from '@utils/use_validation_of_products.ut
 import arrowNextSvg from '@svgs/arrow_next.svg';
 import arrowPrevSvg from '@svgs/arrow_prev.svg';
 
-export const paginationPrevButton = `<button class='shop__products-pagination__button' name='prev-button'>${arrowPrevSvg} </button>`;
-export const paginationNextButton = `<button class='shop__products-pagination__button' name='next-button'>${arrowNextSvg} </button>`;
+const paginationPrevButton = `<button class='shop__products-pagination__button' name='prev-button'>${arrowPrevSvg} </button>`;
+const paginationNextButton = `<button class='shop__products-pagination__button' name='next-button'>${arrowNextSvg} </button>`;
 
-export const set_shop_propertyies = (allProductsArr) => {
+export const set_shop_propertyies = (allProductsArr, updatedPageNumber) => {
   const shopProductsPaginationNode = document.querySelector('.shop__products-pagination');
 
   const sidebarProductCategoriesNode = document.querySelector('.sidebar-content-of-product-categories');
@@ -17,10 +17,12 @@ export const set_shop_propertyies = (allProductsArr) => {
   const shopProductsUtilsResultCounterNode = document.querySelector('.shop__products-utils__result-counter');
   const shopProductsContainerNode = document.querySelector('.shop__products-row');
 
-  const pageNumber = +window.sessionStorage.getItem('pageNumber') || 1;
+  const pageNumber = updatedPageNumber || +window.sessionStorage.getItem('pageNumber') || 1;
   const PRODUCTS_LENGTH_PER_PAGE = 4;
   const END_PRODUCT_IDX = pageNumber * PRODUCTS_LENGTH_PER_PAGE;
   const START_PRODUCT_IDX = END_PRODUCT_IDX - PRODUCTS_LENGTH_PER_PAGE;
+
+  const LAST_PAGE_NUMBER = Math.ceil(allProductsArr.length / PRODUCTS_LENGTH_PER_PAGE);
 
   const [html, categoriesArr, colorsArr, sizeArr, max, min] = use_validation_of_products(
     allProductsArr,
@@ -62,20 +64,35 @@ export const set_shop_propertyies = (allProductsArr) => {
     END_PRODUCT_IDX > allProductsArr.length ? allProductsArr.length : END_PRODUCT_IDX
   } of ${allProductsArr.length} results </span>`;
 
-  if ([...shopProductsPaginationNode.children].length > 2) return;
+  const paginationButtonArr = [...shopProductsPaginationNode.children];
+
+  if (pageNumber == 1 && paginationButtonArr[0].name === 'prev-button') {
+    paginationButtonArr[0].remove();
+  }
+
+  if (paginationButtonArr.every(({ name }) => name !== 'prev-button')) {
+    shopProductsPaginationNode.insertAdjacentHTML('afterbegin', paginationPrevButton);
+  }
+  if (pageNumber == LAST_PAGE_NUMBER && paginationButtonArr[paginationButtonArr.length - 1].name === 'next-button') {
+    paginationButtonArr[paginationButtonArr.length - 1].remove();
+  }
+
+  if (paginationButtonArr.every(({ name }) => name !== 'next-button')) {
+    shopProductsPaginationNode.insertAdjacentHTML('beforeend', paginationNextButton);
+  }
+
+  if ([...shopProductsPaginationNode.children].length > 3) return;
 
   const pagintaionButtonsArr = Array.from(
-    { length: Math.round(allProductsArr.length / PRODUCTS_LENGTH_PER_PAGE) },
+    { length: LAST_PAGE_NUMBER },
     (__, idx) =>
       `<button  name='${idx + 1}'   class='shop__products-pagination__button ${
         pageNumber === idx + 1 ? 'shop__products-pagination__button--active' : ''
       }'>${idx + 1} </button>`
   );
-
   shopProductsPaginationNode.innerHTML = [
-    pageNumber !== 1 ? paginationPrevButton : '',
+    pageNumber != 1 ? paginationPrevButton : '',
     ...pagintaionButtonsArr,
-    paginationNextButton,
+    pageNumber != LAST_PAGE_NUMBER ? paginationNextButton : '',
   ].join('');
-  
 };
