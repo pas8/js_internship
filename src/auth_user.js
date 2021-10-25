646fa73c90fa9ef6cec5bc996ef51b5f21c9cc68
pas8 <whyiampas@gmail.com>
2021-10-25 20:33:32 +0300

    added startup auth_user page

---
+++ src/auth_user.js
@@ -0,0 +1,7 @@
+// import { use_xml_http_request } from '@utils/use_xml_http_request.util.js';
+
+import 'regenerator-runtime/runtime.js';
+(async () => {
+  const token = window.location.search;
+  console.log(token);
+})();
